53f80fd5bbfc4ecbb848206e22b489aa1ac633df
mario <qiujiafei123@gmail.com>
2023-11-09 23:08:56 +0800

    temp: Login 组件测试

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import Login from "./views/login/Login.vue";
 </script>
 
 <template>
@@ -11,6 +12,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <Login />
   <HelloWorld msg="Vite + Vue" />
 </template>
 
